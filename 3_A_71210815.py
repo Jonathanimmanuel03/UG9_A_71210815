604d41c4700ab648ebda1160e34f8c5674332263
jonathan <jonathanimmanuel03@gmail.com>
2021-11-12 03:01:30 +0800

    soal 3

---
+++ 3_A_71210815.py
@@ -0,0 +1,12 @@
+data1 = 'teh'
+data2 = 'susu'
+
+d1 = input(str('Masukan Daftar Belanjaan 1 : '))
+d2 = input(str('Masukan Daftar Belanjaan 2 : '))
+
+d3 = input(str('Tambahkan Daftar Belanja 1 : '))
+d4 = input(str('Tambahkan Daftar Belanja 2 : '))
+
+print('Daftar Belanjaan 1 : ',d1,'dan',d3)
+print('Daftar Belanjaan 2 : ',d2,'dan',d4)
+
